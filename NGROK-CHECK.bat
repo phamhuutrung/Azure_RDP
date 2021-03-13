e523f6c5e28962e3cb1de5c34a8c82218a4dd061
phamhuutrung <79510043+phamhuutrung@users.noreply.github.com>
2021-03-13 16:50:57 +0700

    Update NGROK-CHECK.bat

--- NGROK-CHECK.bat
+++ NGROK-CHECK.bat
@@ -18,5 +18,5 @@ echo All done! Connect your VM using RDP. When RDP expired and VM shutdown, Re-r
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Can't get NGROK tunnel, please paste new NGROK TOKEN in YML. Check Tunnel here: https://dashboard.ngrok.com/status/tunnels " && exit
 echo User: administrator
-echo Pass: ******
+echo Pass: VTVGIAITRI
 ping -n 999999 10.10.0.10 >nul
