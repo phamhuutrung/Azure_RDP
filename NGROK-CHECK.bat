35061aa9e3ae58ddbe772fc580efe6bf5baadc0c
phamhuutrung <79510043+phamhuutrung@users.noreply.github.com>
2021-03-13 16:59:47 +0700

    Update NGROK-CHECK.bat

--- NGROK-CHECK.bat
+++ NGROK-CHECK.bat
@@ -2,7 +2,7 @@
 del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
 net config server /srvcomment:"Windows Azure VM" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
-net user administrator VTVGIAITRI /add >nul
+net user administrator VTV-GT360 /add >nul
 net localgroup administrators administrator /add >nul
 net user administrator /active:yes >nul
 echo To change another VM region, Create New organization (Your current VM location:  %LO% )
