e55d3ccc0a1e14843ec11ab2e85f0b7d46745888
phamhuutrung <79510043+phamhuutrung@users.noreply.github.com>
2021-03-13 15:47:55 +0700

    Update NGROK-CHECK.bat

--- NGROK-CHECK.bat
+++ NGROK-CHECK.bat
@@ -19,6 +19,4 @@ echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Can't get NGROK tunnel, please paste new NGROK TOKEN in YML. Check Tunnel here: https://dashboard.ngrok.com/status/tunnels " && exit
 echo User: administrator
 echo Pass: THTH-channel
-echo View more: https://youtube.com/c/tinhocthuchanh
-echo Visit Create VPS-RDP FREE ALL: https://www.youtube.com/playlist?list=PLMAkivgExRAQi9Mb2_uJfDoMYvdwQXPY-
 ping -n 999999 10.10.0.10 >nul
